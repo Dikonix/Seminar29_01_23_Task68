a06f7e1c7f423f7f1eea2aa4b709f762159f3f98
Diana Brik <di.brik@yandex.kz>
2023-02-03 21:30:21 +0600

    Seminar 29.02.23 Task 68

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0))
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else if ((m > 0) && (n > 0))
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+    else
+    {
+        return n + 1;
+    }
+}
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = Ackermann(m, n);
+Console.WriteLine($"Ackermann({m}, {n}) = {result}");
